a1a9577b2fd3efd12827a1501f66e6a4eef25bc9
Awano Yasutaka <eewano@gmail.com>
2016-10-03 19:52:23 +0900

    1: ステージ01を作成中。

---
+++ Assets/Scripts/DebugCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class DebugCamera : MonoBehaviour {
+
+    private Vector3 cameraPos;
+    [SerializeField]
+    private float moveSpeed = 0.02f;
+
+    void Start() {
+        cameraPos = transform.localPosition;
+    }
+
+    void Update() {
+        cameraPos.x += moveSpeed;
+        transform.localPosition = cameraPos;
+    }
+}
